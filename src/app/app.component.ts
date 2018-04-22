970e5c0fa06e3d907e8ed0b7c57a7dc9e9efb906
Jaszui <dariawisniewska96@gmail.com>
2018-04-22 22:37:31 +0200

    Add auth in firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'group';
+
+  ngOnInit() {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAcre5fljUX5ZSj6nrn9pB4500Qw3LWiNc',
+    authDomain: 'gamespot-6e9ad.firebaseapp.com'
+  });
+  }
+  // why is unused?
   onNavigate(feature: string) {
-this.loadedFeature = feature;
+  this.loadedFeature = feature;
   }
 }
